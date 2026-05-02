f94476c73a47be7c8830436fb892fbeab8eec4f3
JBlond <leet31337@web.de>
2026-05-02 11:07:13 +0200

    Fix: Log when token are disabled

--- mod_bikeshed.c
+++ mod_bikeshed.c
@@ -111,10 +111,15 @@ static int bikeshed_post_config(apr_pool_t * p, apr_pool_t * plog, apr_pool_t * 
               return 0;
       }
 
-      if (strcasecmp(BikeShedTokensString, "none") == 0) {
-        ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "BikeShed: server signature/tokens successfully disabled.");
+      if (strcasecmp(svrcfg->bikeshed_tokens_string, "none") == 0) {
+        ap_log_error(APLOG_MARK, APLOG_ERR | APLOG_NOERRNO, 0, s,
+          "BikeShed: server signature/tokens successfully disabled."
+        );
       } else {
-        ap_log_rerror(APLOG_MARK, APLOG_NOTICE, 0, r, "BikeShed: server signature/tokens successfully changed to \"%s\".", svrcfg->bikeshed_tokens_string);
+        ap_log_error(APLOG_MARK, APLOG_ERR | APLOG_NOERRNO, 0, s,
+          "BikeShed: server signature/tokens successfully changed to \"%s\".",
+          svrcfg->bikeshed_tokens_string
+        );
       }
     }
 
