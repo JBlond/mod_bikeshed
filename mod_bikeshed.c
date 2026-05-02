d4df2580fa025a7a994b70caa2560c74b236298b
JBlond <leet31337@web.de>
2026-05-02 10:48:29 +0200

    Add: Log when the change was successful

--- mod_bikeshed.c
+++ mod_bikeshed.c
@@ -110,11 +110,11 @@ static int bikeshed_post_config(apr_pool_t * p, apr_pool_t * plog, apr_pool_t * 
                          svrcfg->bikeshed_tokens_string);
               return 0;
       }
-    }
 
-    ap_log_error(APLOG_MARK, APLOG_NOTICE | APLOG_NOERRNO, 0, s,
+      ap_log_error(APLOG_MARK, APLOG_NOTICE | APLOG_NOERRNO, 0, s,
         "BikeShed: server signature/tokens successfully changed to \"%s\".",
         svrcfg->bikeshed_tokens_string);
+    }
 
     return OK;
 }
